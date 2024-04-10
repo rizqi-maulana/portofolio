9f762a6b7bc81eac917d7559ed57850c026acc19
Maulanya <maulanarizq26@gmail.com>
2024-04-10 20:35:28 +0800

    Remove class Container in Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} md:container mx-auto`}>
+      <body className={poppins.className}>
         <GetPassword>
           {children}
         </GetPassword>
--- src/components/Contributions/OverviewItem.tsx
+++ src/components/Contributions/OverviewItem.tsx
@@ -8,10 +8,10 @@ interface OverviewItemProps {
 
 const OverviewItem = ({ label, value, unit = '' }: OverviewItemProps) => (
     <Card className='flex flex-col self-center rounded-xl  py-3 px-4 border border-neutral-900'>
-        <span className='md:text-sm sm:text-xs text-neutral-400'>{label}</span>
+        <span className='md:text-sm lg:text-xl text-xs text-neutral-400'>{label}</span>
         <div className='flex items-center gap-2'>
-            <div className='text-sm lg:text-2xl font-medium text-green-600'>{value}</div>
-            {unit && <span className='md:text-sm sm:text-xs text-neutral-400'> {unit}</span>}
+            <div className='md:text-sm lg:text-xl text-xs font-medium text-green-600'>{value}</div>
+            {unit && <span className='md:text-sm lg:text-xl text-xs text-neutral-400'> {unit}</span>}
         </div>
     </Card>
 );
