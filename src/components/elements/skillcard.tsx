0f0fb25d0fdee4ec4097b26b3d34d4c5fcb03779
Maulanya <maulanarizq26@gmail.com>
2024-10-01 12:51:58 +0800

    style: fix slider of skill card

--- src/components/elements/skillcard.tsx
+++ src/components/elements/skillcard.tsx
@@ -1,25 +1,50 @@
 "use client";
-import javascript from '@/assets/image/javascript-svgrepo-com.svg'
+// import javascript from '@/assets/image/javascript-svgrepo-com.svg'
 import Image from 'next/image';
-import { cn } from "@/utils/cn";
-import React, { useEffect, useState, useCallback } from "react";
+import { cn } from '@/utils/cn';
+import React, { useEffect, useState } from "react";
 
 export const Skillcard = ({
     items,
     direction = "left",
+    // speed = "fast",
     pauseOnHover = true,
+    className,
 }: {
     items: {
         name: string;
         icon: string;
     }[];
     direction?: "left" | "right";
+    speed?: "fast" | "normal" | "slow";
     pauseOnHover?: boolean;
     className?: string;
 }) => {
     const containerRef = React.useRef<HTMLDivElement>(null);
     const scrollerRef = React.useRef<HTMLUListElement>(null);
-    const getDirection = useCallback(() => {
+
+    useEffect(() => {
+        addAnimation();
+    }, []);
+    const [start, setStart] = useState(false);
+    function addAnimation() {
+        if (containerRef.current && scrollerRef.current) {
+            const scrollerContent = Array.from(scrollerRef.current.children);
+            containerRef.current.style.setProperty("--animation-duration", "80s");
+
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                if (scrollerRef.current) {
+                    scrollerRef.current.appendChild(duplicatedItem);
+                }
+            });
+
+            getDirection();
+            // getSpeed();
+            setStart(true);
+        }
+    }
+    const getDirection = () => {
         if (containerRef.current) {
             if (direction === "left") {
                 containerRef.current.style.setProperty(
@@ -33,32 +58,7 @@ export const Skillcard = ({
                 );
             }
         }
-    }, [containerRef, direction]);
-    useEffect(() => {
-        function addAnimation() {
-            if (containerRef.current && scrollerRef.current) {
-                containerRef.current.style.setProperty("--animation-duration", "20s");
-                // const scrollerContent = Array.from(scrollerRef.current.children);
-
-                // Duplicate Item
-
-                // scrollerContent.forEach((item) => {
-                //     const duplicatedItem = item.cloneNode(true);
-                //     if (scrollerRef.current) {
-                //         scrollerRef.current.appendChild(duplicatedItem);
-                //     }
-                // });
-
-                getDirection();
-                // getSpeed();
-                setStart(true);
-            }
-        }
-        addAnimation();
-    }, [getDirection]);
-    const [start, setStart] = useState(false);
-
-
+    };
     // const getSpeed = () => {
     //     if (containerRef.current) {
     //         if (speed === "fast") {
@@ -75,25 +75,26 @@ export const Skillcard = ({
             ref={containerRef}
             className={cn(
                 "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+                className
             )}
         >
             <ul
                 ref={scrollerRef}
                 className={cn(
-                    " flex w-max shrink-0 gap-4 py-2 !px-10 md:!px-32 flex-nowrap",
-                    start && "animate-scroll",
+                    " flex min-w-full shrink-0 gap-4 py-2 w-max flex-nowrap",
+                    start && "animate-scroll ",
                     pauseOnHover && "hover:[animation-play-state:paused]"
                 )}
             >
-                {items.map((item) => (
+                {items.map((item, idx) => (
                     <li
-
-                        key={`${item.icon}${Math.random()}`}
+                        className="w-max"
+                        key={item.name}
                     >
                         <div className='bg-[#151527] w-max p-2 rounded-[5px] flex items-center'>
                             <Image
                                 key={item.icon}
-                                src={item.icon || javascript}
+                                src={item.icon}
                                 className="md:w-7 md:h-7 w-[15px] h-[15px]"
                                 width={28}
                                 height={28}
