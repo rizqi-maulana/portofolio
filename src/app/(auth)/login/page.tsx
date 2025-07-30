2e6e118124ee6fa3f6b1b57a3f3e529365b8b8e0
rizqi-maulana <maulanarizq26@gmail.com>
2025-07-30 14:11:20 +0800

    fix: delete login element path

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,9 @@
-import { Metadata } from "next"
-import LoginElement from "../../../components/template/login/LoginElement";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Login"
+  title: "Login",
 };
 
 export default function Login() {
-    return (
-        <LoginElement />
-    )
+  return <LoginElement />;
 }
