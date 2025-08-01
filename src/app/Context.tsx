adc14f60c123ae44f3dd4bb1151e482b57187b22
rizqi-maulana <maulanarizq26@gmail.com>
2025-08-01 08:54:04 +0800

    fix: gradient

--- src/app/Context.tsx
+++ src/app/Context.tsx
@@ -10,13 +10,13 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
         className={cn(
           "fixed inset-0 -z-50",
           "[background-size:40px_40px]",
-          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+          // "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
         )}
       />
 
       {/* Optional masking effect */}
-      <div className="pointer-events-none fixed inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black -z-40" />
+      <div className="pointer-events-none fixed inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] bg-black -z-40" />
 
       {children}
     </UserContext.Provider>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} dark`}>
+      <body className={`${poppins.className}`}>
         <UserProvider>
           <Header />
           {children}
